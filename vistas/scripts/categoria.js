6a70b875b45c06c4deb77ddf01a0e1d64f30f6f9
Elvis <davis-elvis@hotmail.com>
2025-01-29 09:50:04 -0500

    listar

--- vistas/scripts/categoria.js
+++ vistas/scripts/categoria.js
@@ -38,6 +38,37 @@ function cancelarform(){
     mostrarform(false);
 }
 //implementamos la funcion listar los registros de la tabla categoria
+function listar(){
+    tabla=$('#tbllistado').dataTable({
+        //Activamos el procesamiento del datatable
+        "aProcessing": true,
+        //Paginación y filtrado realizado por el servidor
+        "aServiceSide": true,
+        //Definimos lo elementos del control de la tabla 
+        dom: 'Brtilp',
+        //Definimos los botones de exportación 
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdf'
+        ],
+        //definimos o llenamos la tabla con los registros de la base de datos
+        "ajax":
+        {
+            url: '../ajax/categoria.php?op=listar',
+            type: "get",
+            dataType : "json",
+            error : function(e){
+                console.timeLog(e.responseText);
+            }
+        },
+        "bDestroy":true,
+        "iDisplayLength":5,//Paginación
+        //Ordene los datos de forma descendente
+        "order": [[0,"desc"]] //ordenar la columna, orden 
+    }).DataTable();
+}
 //Implementamos la funcion init que me permite mostrar el fomrulario
 
 init();
